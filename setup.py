69faf85cc22c6d4341d234d2f2501ae2eb253eac
David Steele <dsteele@gmail.com>
2015-04-30 15:27:20 -0400

    Add a setup.py installation script

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+from setuptools import setup
+import splitcpy
+
+setup(name='splitcpy',
+        packages=['splitcpy'],
+        version=splitcpy.__version__,
+        description="Copy a remote file using multiple SSH streams",
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Environment :: Console',
+            'Intended Audience :: End Users/Desktop',
+            'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+            'Natural Language :: English',
+            'Operating System :: POSIX',
+            'Programming Language :: Python',
+            'Topic :: Utilities',
+        ],
+        entry_points={
+            'console_scripts': ['splitcpy=splitcpy.splitcpy:main'],
+        },
+        install_requires=['pexpect',],
+        author="David Steele",
+        author_email="dsteele@gmail.com",
+        url='https://github.com/davesteele/splitcpy',
+    )
