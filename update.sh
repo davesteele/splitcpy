de1145bbbd6d65c1e3103911e49e2dd0a308ce76
David Steele <dsteele@gmail.com>
2015-08-29 00:22:11 -0400

    Fix checksum file formatting

--- update.sh
+++ update.sh
@@ -5,9 +5,11 @@ cp ../splitcpy*deb deb/
 cd deb
 echo MD5Sum >checksums.txt
 md5sum *deb >>checksums.txt
-echo "\nSHA1" >>checksums.txt
+echo >>checksums.txt
+echo "SHA1" >>checksums.txt
 sha1sum *deb >>checksums.txt
-echo "\nSHA256" >>checksums.txt
+echo >>checksums.txt
+echo "SHA256" >>checksums.txt
 sha256sum *deb >>checksums.txt
 echo  >>checksums.txt
 
