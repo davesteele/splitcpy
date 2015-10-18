36305bebb83e4445fe528bc0b9e06faa348c64f9
David Steele <dsteele@gmail.com>
2015-10-17 20:10:14 -0400

    Fix tox breakage
    Don't know what caused this - maybe an upstream change to the member
    variables to TestCommand, to '@propertys'.

--- setup.py
+++ setup.py
@@ -16,8 +16,8 @@ class PyTest(TestCommand):
 
     def finalize_options(self):
         TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
+#        self.test_args = []
+#        self.test_suite = True
 
     def run_tests(self):
         import pytest
