010197d52aecf8ff2a3303409a22dd433c664331
David Steele <dsteele@gmail.com>
2015-11-11 20:41:47 -0500

    Try pytest fixture parameters

--- test/test_dl_file.py
+++ test/test_dl_file.py
@@ -8,15 +8,20 @@ import os
 import splitcpy
 
 
-@pytest.fixture()
+filesize = 0
+
+
+@pytest.fixture(params=[128, 256])
 def testfile(request):
+
+    request.module.filesize = request.param
 
     (fd, path) = tempfile.mkstemp()
 
     with open(path, 'wb') as fp:
-        fp.write(bytearray(range(256)))
+        fp.write(bytearray(range(request.param)))
 
-    request.addfinalizer(lambda : os.unlink(path))
+    request.addfinalizer(lambda: os.unlink(path))
 
     return path
 
@@ -50,7 +55,7 @@ def test_dl_slice(del_fifo, mkfifo, process, testfile):
     splitcpy.dl_slice('user@host:file', 2, 0, 1, queue, 'shhh', 22)
 
     assert processmock.poll.called
-    assert queue.put.call_count == 256 + 1
+    assert queue.put.call_count == filesize + 1
     assert processmock.kill.called
     assert mkfifo.call_count == 1
     assert del_fifo.call_count == 1
